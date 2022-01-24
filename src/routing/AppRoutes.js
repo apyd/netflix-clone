9675be480425a7090ed81e57948363448c16065e
apyd <arturpydych@gmail.com>
2022-01-24 19:12:53 +0100

    App routes

---
+++ src/routing/AppRoutes.js
@@ -0,0 +1,44 @@
+import HomeScreen from "../pages/HomeScreen/HomeScreen"
+import Login from '../pages/Login/Login'
+import Register from '../pages/Register/Register'
+import Movies from '../pages/Movies/Movies'
+import TVSeries from '../pages/TVSeries/TVSeries'
+import MyList from '../pages/MyList/MyList'
+import Payments from '../pages/Payments/Payments'
+import Preferences from '../pages/Preferences/Preferences'
+
+
+export const AppRoutes = {
+    HOME: {
+        path: '/',
+        component: HomeScreen()
+    },
+    LOGIN: {
+        path: '/login',
+        component: Login()
+    },
+    REGISTER: {
+        path: '/register',
+        component: Register()
+    },
+    MOVIES: {
+        path: '/movies',
+        component: Movies()
+    },
+    TVSERIES: {
+        path: '/tvseries',
+        component: TVSeries()
+    },
+    MYLIST: {
+        path: '/my-list',
+        component: MyList()
+    },
+    PAYMENTS: {
+        path: '/payments',
+        component: Payments()
+    },
+    PREFERENCES: {
+        path: '/preferences',
+        component: Preferences()
+    },
+}
