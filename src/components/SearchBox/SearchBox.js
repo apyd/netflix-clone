a23225d3151fac42ea7710ec849df4c134976acc
apyd <arturpydych@gmail.com>
2022-01-25 11:37:34 +0100

    Style changes to searchbox and dropdown

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -3,13 +3,15 @@ import styles from './SearchBox.module.css'
 import { useState } from 'react'
 
 function SearchBox({ placeholder }) {
-    const [isFocused, setIsFocused] = useState(true);
+    const [isFocused, setIsFocused] = useState(false);
     return (
         <div className={styles.searchBox}>
-            {isFocused &&
-                <input type="search" placeholder='This is placeholder' className={styles.searchBox__input} />}
+            {isFocused && <label htmlFor="search">
+                <input id="search" name="search" type="search" placeholder={placeholder} className={styles.searchBox__input} />
+            </label>
+            }
             <SearchIcon className={[styles['icon'], styles['icon--small']].join(' , ')} onClick={() => setIsFocused(prev => prev = !prev)} />
-        </div>)
+        </div >)
 }
 
 export default SearchBox;
