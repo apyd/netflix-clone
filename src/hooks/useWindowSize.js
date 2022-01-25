575168429df0afd80de2d4a6a3234c5026b9f770
apyd <arturpydych@gmail.com>
2022-01-25 11:45:19 +0100

    hook useWindowSize changes

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 export const useWindowSize = () => {
     const isClient = typeof window === 'object';
-    function getWidth() {
+    const getWidth = useCallback(() => {
         return isClient ? window.innerWidth : undefined
-    }
-    function getHeight() {
+    }, [isClient])
+    const getHeight = useCallback(() => {
         return isClient ? window.innerHeight : undefined
-    }
+    }, [isClient])
 
     const [windowWidth, setWindowWidth] = useState(getWidth);
     const [windowHeight, setWindowHeight] = useState(getHeight);
@@ -20,6 +20,6 @@ export const useWindowSize = () => {
         }
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize)
-    }, [])
+    }, [isClient, getWidth, getHeight])
     return { windowWidth, windowHeight }
 }
