59f1a16c323cd12db173fa880706219049805695
apyd <arturpydych@gmail.com>
2022-01-30 20:24:08 +0100

    useMediaDetails hook

---
+++ src/hooks/useMediaDetails.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import { mediaType, apiQueries } from '../api/api'
+
+export const useMediaDetails = (type, id) => {
+    const [mediaDetails, setMediaDetails] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = type === mediaType.movie ? apiQueries.getMovieDetails(id) : apiQueries.getTVDetails(id);
+            const response = await fetch(url)
+            const result = await response.json();
+            setMediaDetails({
+                type,
+                title: result.original_title,
+                img: `${apiQueries.getOriginalImage(result.poster_path)}`,
+                date: result.release_date,
+                description: result.overview,
+                genres: result.genres,
+                runtime: result.runtime,
+            });
+        }
+        fetchData();
+    }, [type.id])
+    return mediaDetails
+}
